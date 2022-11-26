30e22d0b1e85291421033fa4118e7c3493fe0176
SimeonVasilev <simvas@abv.bg>
2022-11-26 11:54:33 +0200

    Added CategoryController

---
+++ ChristmasOnlineWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using ChristmasOnlineWeb.Data;
+using ChristmasOnlineWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChristmasOnlineWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
+        }
+    }
+}

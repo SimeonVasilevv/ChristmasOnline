8d827113ec36720e8e8b163de69e5b5ad927fb38
SimeonVasilev <simvas@abv.bg>
2022-11-26 11:16:04 +0200

    Added DbContext

---
+++ ChristmasOnlineWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ChristmasOnlineWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChristmasOnlineWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}

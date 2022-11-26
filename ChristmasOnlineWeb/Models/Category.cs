d5c25727ae062a8d85c7d8e2f16b0e8839452ce8
SimeonVasilev <simvas@abv.bg>
2022-11-26 11:00:47 +0200

    Add project files.

---
+++ ChristmasOnlineWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChristmasOnlineWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public string Id { get; set; } = new Guid().ToString(); //string заради базата ли е? Ще приеме ли GUID?
+        [Required]
+        [StringLength(40,MinimumLength = 4,ErrorMessage ="{0} must be between {2} and {1} symbols")]
+        public string Name { get; set; }
+        [Required]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ ChristmasOnlineWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChristmasOnlineWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

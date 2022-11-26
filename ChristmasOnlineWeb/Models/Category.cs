49e2235744e3754b4d9b691aeb06ca45e3b5dc87
SimeonVasilev <simvas@abv.bg>
2022-11-26 11:17:31 +0200

    Added ApplicationDbContext in Program.cs. Added configuration for connection string

--- ChristmasOnlineWeb/Models/Category.cs
+++ ChristmasOnlineWeb/Models/Category.cs
@@ -9,7 +9,6 @@ namespace ChristmasOnlineWeb.Models
         [Required]
         [StringLength(40,MinimumLength = 4,ErrorMessage ="{0} must be between {2} and {1} symbols")]
         public string Name { get; set; }
-        [Required]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
